ca3d19075423f8dd00199b0388d3aa0220c335f5
thuan3598 <vhieu5431@gmail.com>
2024-04-05 01:47:17 +0700

    create form login

---
+++ src/containers/Auth/login.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+
+import * as actions from "../../store/actions";
+
+import "./login.scss";
+import { FormattedMessage } from "react-intl";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="login-background">
+        <div className="login-container">
+          <div className="login-content row">
+            <div className="col-12 text-login">Login</div>
+            <div className="col-12 form-group login-input">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your username"
+              />
+            </div>
+            <div className="col-12 form-group login-input">
+              <label>Password</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your password"
+              />
+            </div>
+            <div className="col-12 ">
+              <button className="btn-login">Login</button>
+            </div>
+            <div className="col-12 forgot-password">
+              <span>Forgot your password?</span>
+            </div>
+            <div className="col-12 text-center mt-3">
+              <span className="text-other-login">Or login with:</span>
+            </div>
+            <div className="col-12 social-login">
+              <i className="fab fa-google-plus-g"></i>
+              <i className="fab fa-facebook-f"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    navigate: (path) => dispatch(push(path)),
+    adminLoginSuccess: (adminInfo) =>
+      dispatch(actions.adminLoginSuccess(adminInfo)),
+    adminLoginFail: () => dispatch(actions.adminLoginFail()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
