be44ff85c352c18ddaecf7ad17ebdc33ac3bf909
thuan3598 <vhieu5431@gmail.com>
2024-04-16 22:48:18 +0700

    #40 CRUD React 4 - Design Modal Thêm Mới Người Dùng Với Reactstrap

---
+++ src/containers/System/ModalUser.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+
+class ModalUser extends Component {
+  componentDidMount() {}
+
+  toggle = () => {
+    this.props.toggleFromParent();
+  };
+
+  render() {
+    return (
+      <Modal
+        isOpen={this.props.isOpen}
+        toggle={() => {
+          this.toggle();
+        }}
+        className="modal-user-container"
+        size="lg"
+        centered
+      >
+        <ModalHeader
+          toggle={() => {
+            this.toggle();
+          }}
+        >
+          Create New User
+        </ModalHeader>
+        <ModalBody>
+          <div className="modal-user-body">
+            <div className="input-container">
+              <label>Email</label>
+              <input type="text"></input>
+            </div>
+            <div className="input-container">
+              <label>Password</label>
+              <input type="password"></input>
+            </div>
+            <div className="input-container">
+              <label>FirstName</label>
+              <input type="text"></input>
+            </div>
+            <div className="input-container">
+              <label>LastName</label>
+              <input type="password"></input>
+            </div>
+            <div className="input-container max-width-input">
+              <label>Address</label>
+              <input type="password"></input>
+            </div>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            color="primary"
+            onClick={() => {
+              this.toggle();
+            }}
+            className="px-3"
+          >
+            Save changes
+          </Button>{" "}
+          <Button
+            color="secondary"
+            onClick={() => {
+              this.toggle();
+            }}
+            className="px-3"
+          >
+            Close
+          </Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ModalUser);
--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,12 +3,13 @@ import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import "./usersManage.scss";
 import { getAllUsers } from "../../services/userService";
-
+import ModalUser from "./ModalUser";
 class UserManage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       arrUsers: [],
+      isOpenModalUser:false,
     };
   }
 
@@ -20,12 +21,33 @@ class UserManage extends Component {
       });
     }
   }
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModalUser:true
+    })
+  };
+
+  toggleUserModal = () => {
+    this.setState({
+      isOpenModalUser:!this.state.isOpenModalUser
+    })
+  };
 
   render() {
     let arrUsers = this.state.arrUsers;
     return (
       <div className="users-container">
+        <ModalUser
+          isOpen={this.state.isOpenModalUser}
+          toggleFromParent={this.toggleUserModal}
+        />
         <div className="title text-center">manage users</div>
+        <div className="mx-1">
+          <button className="btn btn-primary px-3" onClick={()=>this.handleAddNewUser()}>
+            <i className="fas fa-plus"></i>
+            Add user
+          </button>
+        </div>
         <div className="users-table mt-3 mx-1">
           <table id="customers">
             <tr>
@@ -35,23 +57,26 @@ class UserManage extends Component {
               <th>Address</th>
               <th>Actions</th>
             </tr>
-            
-              {arrUsers &&
-                arrUsers.map((item, index) => {
-                  return (
-                    <tr key={index}>
-                      <td>{item.email}</td>
-                      <td>{item.firstName}</td>
-                      <td>{item.lastName}</td>
-                      <td>{item.address}</td>
-                      <td>
-                        <button className="btn-edit"><i className="fas fa-pencil-alt"></i></button>
-                        <button className="btn-delete"><i className="fas fa-trash-alt"></i></button>
-                      </td>
-                    </tr>
-                  );
-                })}
-            
+
+            {arrUsers &&
+              arrUsers.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.email}</td>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.address}</td>
+                    <td>
+                      <button className="btn-edit">
+                        <i className="fas fa-pencil-alt"></i>
+                      </button>
+                      <button className="btn-delete">
+                        <i className="fas fa-trash-alt"></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
           </table>
         </div>
       </div>
