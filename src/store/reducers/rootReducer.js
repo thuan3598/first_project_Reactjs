414174134ad2d7bc05cfda2bbb4eb7c0dda4525f
thuan3598 <vhieu5431@gmail.com>
2024-05-01 19:59:59 +0700

    #54 Sửa lỗi lưu ngôn ngữu khi reload

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -18,12 +18,18 @@ const persistCommonConfig = {
 const userPersistConfig = {
     ...persistCommonConfig,
     key: 'user',
-    whitelist: ['isLoggedIn', 'userInfo','language']
+    whitelist: ['isLoggedIn', 'userInfo']
 };
+
+const appPersisConfig={
+    ...persistCommonConfig,
+    key:'app',
+    whitelist:['language']
+}
 
 export default (history) => combineReducers({
     router: connectRouter(history),
     user: persistReducer(userPersistConfig, userReducer),
    
-    app: appReducer
+    app: persistReducer(appPersisConfig,appReducer)
 })
