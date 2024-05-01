9736a3ea76d62f7328c459d1424617a4fb0c2995
thuan3598 <vhieu5431@gmail.com>
2024-05-01 21:18:41 +0700

    #54 Phép Màu Khiến Redux Là Lựa Chọn Number 1 với React App

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import { FormattedMessage } from "react-intl";
 import * as actions from "../../store/actions";
 import Navigator from "../../components/Navigator";
 import { adminMenu } from "./menuApp";
 import "./Header.scss";
 import { LANGUAGES } from "../../utils";
+import HomeHeader from "../HomePage/HomeHeader";
 
 class Header extends Component {
   handleChangeLanguage = (language) => {
     this.props.changeLanguageAppRedux(language);
   };
   render() {
-    let language=this.props.language;
+    // let language=this.props.language;
 
-    const { processLogout } = this.props;
-
+    const { processLogout,language,userInfo } = this.props;
+    console.log(userInfo);
     return (
       <div className="header-container">
         {/* thanh navigator */}
@@ -23,6 +24,7 @@ class Header extends Component {
           <Navigator menus={adminMenu} />
         </div>
         <div className="languages">
+          <span className="welcome"><FormattedMessage id="homeheader.welcome"/>,{userInfo&&userInfo.firstName?userInfo.firstName:' '}</span>
           <span
             className={language===LANGUAGES.VI?'language-vi active':'language-vi'}
             onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
@@ -53,6 +55,7 @@ class Header extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    userInfo:state.user.userInfo,
     language: state.app.language,
 
   };
